c5d0a3e27abfde5b7d29864e2ec487f9eb2a8be8
VINAYREDDY-VIT <55698322+VINAYREDDY-VIT@users.noreply.github.com>
2019-09-23 18:50:02 +0530

    Add files via upload

---
+++ vit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class vit{
+	public static void main(String[] main){
+		Scanner in = new Scanner(System.in);
+		int n= in.nextInt();
+		int fn=2*n-1;
+		int i,j;
+		int c= fn-1;
+		for(i=0;i<fn;i++){
+			for(j=0;j<fn;j++){
+				if(i==0 || i==fn-1 || j==0 || j==fn-1 || i==j || j==c)
+					System.out.print("*  ");
+				else
+					System.out.print("   ");
+			}
+			c--;
+			System.out.println("");
+		}
+	}
+}
